ea966bbb34e1ecc0e995033f04a11423429b7a4e
Jin <mmm1103@naver.com>
2022-11-23 23:01:18 +0900

    feat: get data from open api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Main from './pages/main';
+import Sever from './componets/Sever';
 function App() {
-  return <div></div>;
+    return (
+        <div>
+            <Main />
+            <Sever />
+        </div>
+    );
 }
 
 export default App;
