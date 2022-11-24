1d73eb5ae4fa716d9074e99b3f6450683e9ddde2
diverJenny <jenny.es2005@gmail.com>
2022-11-24 17:55:37 +0900

    design: 마커 사이즈 변경 작업중

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import icon from "../images/parkingIcon.png";
+import redIcon from "../images/car_mark_red.png"
+import yellowIcon from "../images/car_mark_yellow.png"
+import greenIcon from "../images/car_mark_green.png"
 import styled from "styled-components";
 import SpotList from "../SpotList.json";
 
@@ -7,13 +10,6 @@ function Main() {
   const mapRef = useRef();
   let markers = []; // 마커들의 정보들을 담을 배열
   let infoWindows = []; // 공영 주차장 정보들을 담을 배열
-
-  // const InfoBox = styled.div`
-  //   width: 200px;
-  //   height: 200px;
-  //   text-align: center;
-  //   padding: 10px;
-  // `;
 
   useEffect(() => {
     const { naver } = window;
@@ -25,17 +21,6 @@ function Main() {
 
     const map = new naver.maps.Map(mapRef.current, mapOptions);
 
-    // 지도 처음 위치 마커 표시
-    // let markerOptions = {
-    //   position: new naver.maps.LatLng(37.540765, 126.946055),
-    //   map: map,
-    //   icon: {
-    //     url: icon,
-    //   },
-    // };
-
-    // let marker = new naver.maps.Marker(markerOptions);
-
     // 다중 마커 표시
     for (let key = 0; key < SpotList.length; key++) {
       let position = new naver.maps.LatLng(
@@ -43,15 +28,23 @@ function Main() {
         SpotList[key].lng
       );
 
+      // marker 색상 지정
+      const iconColor = SpotList[key].id%2==0 ? redIcon : yellowIcon;
+      const iconSize = new naver.maps.Size(230, 230);
+
       let marker = new naver.maps.Marker({
         map: map,
         position: position,
         title: key,
         icon: {
-          url: icon, // 현재 사용 중인 마커 이미지
+          url: iconColor, // 현재 사용 중인 마커 이미지
+          size: new naver.maps.Size(230, 230),
+          origin: new naver.maps.Point(0, 0),
+          anchor: new naver.maps.Point(11, 35)
         },
-        // zIndex: 100,
       });
+      marker.setZIndex();
+      console.log(iconSize.toString());
 
       let infoWindow = new naver.maps.InfoWindow({
         content:
@@ -63,23 +56,6 @@ function Main() {
       markers.push(marker);
       infoWindows.push(infoWindow);
     }
-
-    // 클릭한 지점 마커 표시
-    // naver.maps.Event.addListener(map, "click", function (e) {
-    //   marker.setPosition(e.latlng);
-    //   map.setCenter(e.latlng); // 클릭한 지점으르 중심으로 화면 이동
-    //   if (infoWindow.getMap()) {
-    //     infoWindow.close();
-    //   }
-    // });
-
-    // let infoWindow = new naver.maps.InfoWindow({
-    //   content:
-    //     '<div style="width: 200px; text-align: center; padding: 10px"><b>마포교육장소</b><br />- 네이버 지도 -</div>',
-    // });
-
-    // markers.push(marker);
-    // infoWindows.push(infoWindow);
 
     // 마커 클릭 시 주차장 정보 on/off
     const openInfoBox = (seq) => {
@@ -98,7 +74,6 @@ function Main() {
 
     for (let i = 0; i < markers.length; i++) {
       naver.maps.Event.addListener(markers[i], "click", openInfoBox(i)); // 클릭한 마커 핸들러
-      // console.log("marker " + i + "" + markers[i].getPosition());
     }
   }, []);
 
